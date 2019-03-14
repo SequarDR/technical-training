028b096970528d0ff111bb3523102752a3282c9c
r.cammardella <Libellula2018>
2019-03-14 10:40:40 +0100

    m

---
+++ openacademy/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from . import test_academy
+
+
+print('hello 0')
---
+++ openacademy/tests/test_academy.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo.tests.common import TransactionCase
+
+print('hello 1')
+class TestCourse(TransactionCase):
+    print('hello 2')
+
+    def test_action(self):
+        record = self.env['openacademy.course'].search(
+            {
+                'name': 'Dragon Technical Training'
+            })
+        record.update({
+            'description': 'Test1'
+        })
+        self.assertEqual(
+            record.description, 'Tes5t1')
